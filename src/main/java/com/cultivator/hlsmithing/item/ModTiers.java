6e38bcd61f4f893add1ffb177167e619ee48c6c5
cultivator <i.amthepizzaguy@gmail.com>
2024-04-11 20:08:02 -0400

    Updated Rose Gold and Steel tool durability

--- src/main/java/com/cultivator/hlsmithing/item/ModTiers.java
+++ src/main/java/com/cultivator/hlsmithing/item/ModTiers.java
@@ -5,9 +5,9 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
 
 public class ModTiers {
-    public static final ForgeTier STEEL = new ForgeTier(2, 250, 6.0F, 2.0F, 14,
+    public static final ForgeTier STEEL = new ForgeTier(2, 400, 6.0F, 2.0F, 14,
             BlockTags.NEEDS_IRON_TOOL, () -> Ingredient.of(ModItems.STEEL_PLATE.get()));
 
-    public static final ForgeTier ROSE_GOLD = new ForgeTier(0, 32, 12.0F, 0.0F, 22,
-            BlockTags.NEEDS_IRON_TOOL, () -> Ingredient.of(ModItems.ROSE_GOLD_INGOT.get()));
+    public static final ForgeTier ROSE_GOLD = new ForgeTier(0, 60, 12.0F, 0.0F, 22,
+            BlockTags.NEEDS_STONE_TOOL, () -> Ingredient.of(ModItems.ROSE_GOLD_INGOT.get()));
 }
